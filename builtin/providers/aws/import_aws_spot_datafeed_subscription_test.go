f99f602670e5004a9fd4d15e38303f401f81a668
Jake Champlin <jake.champlin.27@gmail.com>
2017-04-17 10:49:43 -0400

    update import test

--- builtin/providers/aws/import_aws_spot_datafeed_subscription_test.go
+++ builtin/providers/aws/import_aws_spot_datafeed_subscription_test.go
@@ -9,18 +9,18 @@ import (
 
 func TestAccAWSSpotDatafeedSubscription_importBasic(t *testing.T) {
 	resourceName := "aws_spot_datafeed_subscription.default"
-	ri := acctest.RandInt()
+	ri := acctest.RandIntRange(1, 50000)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckAWSSpotDatafeedSubscriptionDestroy,
 		Steps: []resource.TestStep{
-			resource.TestStep{
+			{
 				Config: testAccAWSSpotDatafeedSubscription(ri),
 			},
 
-			resource.TestStep{
+			{
 				ResourceName:      resourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
